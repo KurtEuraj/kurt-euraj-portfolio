91455fc2094a8d73e7ee9d7834497600cff5a616
Kurt Euraj <kurteurajdev@gmail.com>
2024-05-07 13:49:00 -0400

    add desktop navigation. add hook to detect screen size

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,15 @@ import { gsap } from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { NavLink } from "react-router-dom"
+import useScreenSize from "../../hooks/useScreenSize";
 
 function Header() {
     gsap.registerPlugin(useGSAP, ScrollTrigger);
     const tl = gsap.timeline();
 
     const [isMobileNavOpen, setIsMobileNavOpen] = useState(false)
+
+    const screenSize = useScreenSize();
 
     const toggleMobileNav = () => {
         setIsMobileNavOpen(!isMobileNavOpen)
@@ -84,32 +87,54 @@ function Header() {
     return (
         <header className="nav-bar">
             <nav className="nav-bar__wrapper">
-                <div className="nav-bar__mobile">
-                    <div className={isMobileNavOpen ? "nav-bar__mobile-icon nav-bar__mobile-icon--open" : "nav-bar__mobile-icon nav-bar__mobile-icon--closed"} onClick={clickMobileNav}>
-                        <div className="nav-bar__mobile-icon-stroke nav-bar__mobile-icon-stroke--top"></div>
-                        <div className="nav-bar__mobile-icon-stroke nav-bar__mobile-icon-stroke--middle"></div>
-                        <div className="nav-bar__mobile-icon-stroke nav-bar__mobile-icon-stroke--bottom"></div>
+                {screenSize.width < 1280 ? (
+                    <div className="nav-bar__mobile">
+                        <div className={isMobileNavOpen ? "nav-bar__mobile-icon nav-bar__mobile-icon--open" : "nav-bar__mobile-icon nav-bar__mobile-icon--closed"} onClick={clickMobileNav}>
+                            <div className="nav-bar__mobile-icon-stroke nav-bar__mobile-icon-stroke--top"></div>
+                            <div className="nav-bar__mobile-icon-stroke nav-bar__mobile-icon-stroke--middle"></div>
+                            <div className="nav-bar__mobile-icon-stroke nav-bar__mobile-icon-stroke--bottom"></div>
+                        </div>
+                        <div className="nav-bar__mobile-wrapper">
+                            <div className="nav-bar__mobile-links-wrapper">
+                                <NavLink onClick={clickMobileNav} to="/" className={({ isActive, isPending }) =>
+                                    isPending ? "nav-bar__mobile-link pending" : isActive ? "nav-bar__mobile-link active" : "nav-bar__mobile-link"
+                                }>
+                                    Home
+                                </NavLink>
+                                <NavLink onClick={clickMobileNav} to="/about" className={({ isActive, isPending }) =>
+                                    isPending ? "nav-bar__mobile-link pending" : isActive ? "nav-bar__mobile-link active" : "nav-bar__mobile-link"
+                                }>
+                                    About Me
+                                </NavLink>
+                                <NavLink onClick={clickMobileNav} to="/projects" className={({ isActive, isPending }) =>
+                                    isPending ? "nav-bar__mobile-link pending" : isActive ? "nav-bar__mobile-link active" : "nav-bar__mobile-link"
+                                }>
+                                    Projects
+                                </NavLink>
+                            </div>
+                        </div>
                     </div>
-                    <div className="nav-bar__mobile-wrapper">
-                        <div className="nav-bar__mobile-links-wrapper">
-                            <NavLink onClick={clickMobileNav} to="/" className={({ isActive, isPending }) =>
-                                isPending ? "nav-bar__mobile-link pending" : isActive ? "nav-bar__mobile-link active" : "nav-bar__mobile-link"
+                ) : (
+                    <div className="nav-bar__desktop">
+                        <div className="nav-bar__desktop-links-wrapper">
+                            <NavLink to="/" id="home-nav-link" className={({ isActive, isPending }) =>
+                                isPending ? "nav-bar__desktop-link pending" : isActive ? "nav-bar__desktop-link active" : "nav-bar__desktop-link"
                             }>
                                 Home
                             </NavLink>
-                            <NavLink onClick={clickMobileNav} to="/about" className={({ isActive, isPending }) =>
-                                isPending ? "nav-bar__mobile-link pending" : isActive ? "nav-bar__mobile-link active" : "nav-bar__mobile-link"
+                            <NavLink to="/about" className={({ isActive, isPending }) =>
+                                isPending ? "nav-bar__desktop-link pending" : isActive ? "nav-bar__desktop-link active" : "nav-bar__desktop-link"
                             }>
                                 About Me
                             </NavLink>
-                            <NavLink onClick={clickMobileNav} to="/projects" className={({ isActive, isPending }) =>
-                                isPending ? "nav-bar__mobile-link pending" : isActive ? "nav-bar__mobile-link active" : "nav-bar__mobile-link"
+                            <NavLink to="/projects" className={({ isActive, isPending }) =>
+                                isPending ? "nav-bar__desktop-link pending" : isActive ? "nav-bar__desktop-link active" : "nav-bar__desktop-link"
                             }>
                                 Projects
                             </NavLink>
                         </div>
                     </div>
-                </div>
+                )}
             </nav>
         </header>
     )
