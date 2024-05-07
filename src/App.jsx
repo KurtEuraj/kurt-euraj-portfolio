c2008289f5fc2f181f21e6deabe0722cc7e1aaf3
Kurt Euraj <kurteurajdev@gmail.com>
2024-05-06 23:35:56 -0400

    add mobile navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header/Header';
+import Home from "./pages/Home/Home";
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
